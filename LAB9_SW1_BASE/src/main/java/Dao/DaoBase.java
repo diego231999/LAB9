acadf5b71ab42484bbc860884be895cc2476dff5
Joham RV <a20181459@pucp.edu.pe>
2020-12-01 19:09:50 -0500

    Dao Base 2

--- LAB9_SW1_BASE/src/main/java/Dao/DaoBase.java
+++ LAB9_SW1_BASE/src/main/java/Dao/DaoBase.java
@@ -16,7 +16,7 @@ public abstract class DaoBase {
 
         String user = "root";
         String pass =  "root";
-        String url = "jdbc:mysql://localhost:3306/grupo1_db?serverTimezone=America/Lima";
+        String url = "jdbc:mysql://localhost:3306/sw1lab8?serverTimezone=America/Lima";
 
         return DriverManager.getConnection(url, pass, user);
     }
